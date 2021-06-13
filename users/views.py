04d4b87d02eec9c435058b746525e4a4b5da972a
hwaya2828 <hwaya2828@gmail.com>
2021-06-13 22:36:08 +0900

    MISC: 줄 띄움 수정

--- users/views.py
+++ users/views.py
@@ -76,7 +76,9 @@ class BatchPageView(View):
                                         'peer_id'                : user.id,
                                         'peer_name'              : user.name,
                                         'peer_profile_image_url' : user.profile_image_url,
-                                        'peer_status'            : 'OFF' if not user.record_set.last() else 'ON' if now_korea.date() == user.record_set.last().start_at.date() and not user.record_set.last().end_at else 'OFF',
+                                        'peer_status'            : 'OFF' if not user.record_set.last() \
+                                                else 'ON' if now_korea.date() == user.record_set.last().start_at.date() \
+                                                and not user.record_set.last().end_at else 'OFF',
                                     } for user in my_batch_users
                                 ]
                 }
