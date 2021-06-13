680804279af6b3ddcd54d601d86c947a29bf57c8
hwaya2828 <hwaya2828@gmail.com>
2021-06-13 14:49:55 +0900

    FEAT: 기수 페이지 API 생성 - query parameter 사용하여 기수별 데이터 전송 - 해당 기수 등록된 학생들의 'ON' 'OFF' 상태 포함

--- users/views.py
+++ users/views.py
@@ -1,65 +1,84 @@
 import json
 import datetime
 
-from django.http    import JsonResponse
-from django.views   import View
+from django.http  import JsonResponse
+from django.views import View
+from datetime     import date
 
-from users.models   import User, Batch
+from users.models import User, Batch
 class BatchPageView(View):
     # @login_confirm
     def get(self, request):
         # user = request.user
         user = User.objects.get(id=1)
-        # batch_id = request.GET.get('batch_id', None)
-        batch_id = 1
+        get_batch_id = request.GET.get('batch_id', None)
 
-        if user.user_type_id == 2:
-            if not user.batch_id == batch_id:
-                return JsonResponse({'message': 'NOT_YOUR_BATCHPAGE_ERROR'}, status = 400)
+        # if user.user_type_id == 2:
+        #     if not user.batch_id == get_batch_id:
+        #         return JsonResponse({'message': 'NOT_YOUR_BATCHPAGE_ERROR'}, status = 400)
 
-        batch    = Batch.objects.get(id=batch_id)
-        users    = User.objects.filter(batch_id=batch.id)
-        now      = datetime.datetime.now()
-        time_gap = datetime.timedelta(seconds=32406)
-        kor_time = now + time_gap
+        my_batch       = Batch.objects.get(id=get_batch_id)
+        my_batch_users = User.objects.filter(batch_id=my_batch.id)
+        now            = datetime.datetime.now()
+        time_gap       = datetime.timedelta(seconds=32406)
+        kor_time       = now + time_gap
 
         batches           = Batch.objects.all()
         batch_total_times = []
         for batch in batches:
-            users = User.objects.filter(batch_id=batch.id)
-            batch_total_times.append(sum([user.total_time for user in users]))
-
-        winner_time  = max(batch_total_times)
-        for_indexing = batch_total_times.index(winner_time)
-        winner_batch = batches[for_indexing]
-        winner_users = User.objects.filter(batch_id=winner_batch.id)
+            each_batch_users = User.objects.filter(batch_id=batch.id)
+            batch_total_times.append(sum([user.total_time for user in each_batch_users]))
+        winner_batch_time = max(batch_total_times)
+        index_number      = batch_total_times.index(winner_batch_time)
+        winner_batch      = batches[index_number]
 
         GOST_RANKING = 3
 
-        if len(users) < GOST_RANKING:
-            ranking_users = users.order_by('-total_time')
+        today               = kor_time.isocalendar()
+        last_week_start_day = date.fromisocalendar(today.year, today.week-1, 1)
+        last_week_end_day   = last_week_start_day + datetime.timedelta(days=6)
+
+        compare_times = []
+        for user in my_batch_users:
+            records              = user.record_set.filter(end_at__date__range=[last_week_start_day, last_week_end_day])
+            last_week_total_time = 0
+            for record in records:
+                last_week_total_time += record.oneday_time
+            compare_times.append(last_week_total_time)
+        
+        ranking_results = []
+        if len(compare_times) < GOST_RANKING:
+            ranking_times = sorted(compare_times, reverse=True)
         else:
-            ranking_users = users.order_by('-total_time')[:GOST_RANKING]
+            ranking_times = sorted(compare_times, reverse=True)[:GOST_RANKING]
+
+        for ranking_time in ranking_times:
+            user_informaion = {}
+            index_number = compare_times.index(ranking_time)
+            user_informaion['user_id']                   = my_batch_users[index_number].id
+            user_informaion['user_name']                 = my_batch_users[index_number].name
+            user_informaion['user_profile_image_url']    = my_batch_users[index_number].profile_image_url
+            user_informaion['user_last_week_total_time'] = ranking_time
+            ranking_results.append(user_informaion)
         
         result = [
             {
                 'winner_batch_information' : {
                                     'winner_batch_name' : winner_batch.name,
-                                    'winner_batch_total_time' : sum([user.total_time for user in winner_users])
+                                    'winner_batch_total_time' : winner_batch_time
                 },
                 'my_batch_information' : {
-                                    'batch_id'         : batch.id,
-                                    'batch_name'       : batch.name,
-                                    'batch_total_time' : sum([user.total_time for user in users]),
-                                    'ghosts'           : {
-                                    },
+                                    'batch_id'         : my_batch.id,
+                                    'batch_name'       : my_batch.name,
+                                    'batch_total_time' : sum([user.total_time for user in my_batch_users]),
+                                    'ghost_ranking'    : ranking_results,
                                     'peers' : [
                                         {
                                             'peer_id'                : user.id,
                                             'peer_name'              : user.name,
                                             'peer_profile_image_url' : user.profile_image_url,
-                                            'peer_status'            : 'ON' if not kor_time.date() == user.record_set.last().start_at.date() and not user.record_set.last().end_at else 'OFF',
-                                        } for user in users
+                                            'peer_status'            : 'OFF' if not user.record_set.last() else 'ON' if not kor_time.date() == user.record_set.last().start_at.date() and not user.record_set.last().end_at else 'OFF',
+                                        } for user in my_batch_users
                                     ]
                 }
             }
