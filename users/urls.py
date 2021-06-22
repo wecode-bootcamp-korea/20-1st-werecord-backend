7149f2973236b5f2ef7cf4f6cf65316e5ef743f2
hwaya2828 <hwaya2828@gmail.com>
2021-06-22 16:46:20 +0900

    FEAT: 기수 페이지 데이터 전송 - 기수별 수강생들의 데이터 전송

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from users.views import GoogleLoginView, StudentView
+from users.views import GoogleLoginView, StudentView, BatchView
 
 urlpatterns = [
     path("/login", GoogleLoginView.as_view()),
     path("/student", StudentView.as_view()),
+    path("/batch/<int:batch_id>", BatchView.as_view()),
 ]
