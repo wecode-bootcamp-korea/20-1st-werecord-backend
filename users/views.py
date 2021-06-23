6116adab2393c16ac224c851b4ab2e91eaaf6e3c
hwaya2828 <hwaya2828@gmail.com>
2021-06-23 10:11:42 +0900

    MISC: 줄바꿈

--- users/views.py
+++ users/views.py
@@ -15,7 +15,41 @@ from django.http       import JsonResponse
 from my_settings       import SECRET, ALGORITHM, JWT_DURATION_SEC
 from werecord.settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
 from users.models      import User, UserType, Batch, Position
-from utils.decorator   import login_required
+from utils.decorator   import login_required 
+
+class GoogleLoginView(View):
+    def get(self, request):
+        id_token     = request.headers.get('Authorization', None)
+
+        if not id_token:
+            return JsonResponse({'message': 'ID_TOKEN_REQUIRED'}, status=401)
+
+        user_request = requests.get(f'https://oauth2.googleapis.com/tokeninfo?id_token={id_token}')
+        user_info    = user_request.json()
+
+        if not user_info:
+            return JsonResponse({'message': 'INVALID_TOKEN'}, status=401)
+
+        login_user, created = User.objects.get_or_create(
+            google_login_id   = user_info['sub'],
+        )
+
+        werecord_token = jwt.encode(
+                {
+                    'user_id'  : login_user.id,
+                    'iat'      : int(time.time()),
+                    'exp'      : int(time.time()) + JWT_DURATION_SEC
+                }, SECRET['secret'],ALGORITHM
+        )
+
+        user_info = {
+            'user_type'        : login_user.user_type.name if login_user.user_type else "",
+            'batch'            : login_user.batch.name if login_user.batch else "",
+            "email'"           : user_info.get('email') if user_info.get('email') else "",
+            "profile_image_url": user_info.get('picture') if user_info.get('picture') else "",
+        }
+
+        return JsonResponse({'user_info' : user_info, 'werecord_token' : werecord_token}, status=200)
 
 class UserInfoView(View):
     s3_client = boto3.client(
@@ -89,47 +123,12 @@ class UserInfoView(View):
         except KeyError:
             return JsonResponse({"message": "KEY_ERROR"}, status=400)     
 
-
     @login_required
     def delete(self, request):
         
         request.user.delete()
         
-        return JsonResponse({"message": "SUCCESS"}, status=204)  
-
-class GoogleLoginView(View):
-    def get(self, request):
-        id_token     = request.headers.get('Authorization', None)
-
-        if not id_token:
-            return JsonResponse({'message': 'ID_TOKEN_REQUIRED'}, status=401)
-
-        user_request = requests.get(f'https://oauth2.googleapis.com/tokeninfo?id_token={id_token}')
-        user_info    = user_request.json()
-
-        if not user_info:
-            return JsonResponse({'message': 'INVALID_TOKEN'}, status=401)
-
-        login_user, created = User.objects.get_or_create(
-            google_login_id   = user_info['sub'],
-        )
-
-        werecord_token = jwt.encode(
-                {
-                    'user_id'  : login_user.id,
-                    'iat'      : int(time.time()),
-                    'exp'      : int(time.time()) + JWT_DURATION_SEC
-                }, SECRET['secret'],ALGORITHM
-        )
-
-        user_info = {
-            'user_type'        : login_user.user_type.name if login_user.user_type else "",
-            'batch'            : login_user.batch.name if login_user.batch else "",
-            "email'"           : user_info.get('email') if user_info.get('email') else "",
-            "profile_image_url": user_info.get('picture') if user_info.get('picture') else "",
-        }
-
-        return JsonResponse({'user_info' : user_info, 'werecord_token' : werecord_token}, status=200)
+        return JsonResponse({"message": "SUCCESS"}, status=204)
 
 class StudentView(View):
     @login_required
