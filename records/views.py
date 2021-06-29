c1cc942c7ad2e137c9ebcea26fc9316e058d00fd
hwaya2828 <hwaya2828@gmail.com>
2021-06-29 16:30:25 +0900

    REFACTOR: 코드 개선

--- records/views.py
+++ records/views.py
@@ -12,9 +12,7 @@ class RecordCheckView(View):
     @login_required
     def get(self, request):
         user        = request.user
-        now         = datetime.datetime.now()
-        time_gap    = datetime.timedelta(seconds=32406)
-        now_korea   = now + time_gap
+        now_korea   = datetime.datetime.now() + datetime.timedelta(seconds=32406)
         record      = user.record_set.last()
         check_date  = record.start_at.date() if record else None
         user_status = False if not record \
@@ -24,10 +22,10 @@ class RecordCheckView(View):
                 return JsonResponse({'message': 'NEED_TO_RECORD_ENDTIME_ERROR', 'result': check_date}, status=400)
             
         result = {
-                'user_id'         : user.id,
-                'user_name'       : user.name,
-                'user_status'     : user_status,
-                'user_start_time' : record.start_at.time() if user_status else None
+                    'user_id'         : user.id,
+                    'user_name'       : user.name,
+                    'user_status'     : user_status,
+                    'user_start_time' : record.start_at.time() if user_status else None
         }
     
         return JsonResponse({'result': result}, status=200)
@@ -36,25 +34,39 @@ class RecordCheckView(View):
     @check_ip
     def post(self, request):
         try:
-            user   = request.user
-            data   = json.loads(request.body)
-            record = user.record_set.last()
-            date   = record.start_at
+            user          = request.user
+            data          = json.loads(request.body)
+            record        = user.record_set.last()
+            standard_time = record.start_at.replace(hour=0, minute=0, second=0, microsecond=0)
 
             if not 0 <= data['hour'] <= 23 or not 0 <= data['minute'] <= 59:
                 return JsonResponse({'message': 'TIME_FORM_ERROR'}, status=400)
 
-            end_at = datetime.datetime(date.year, date.month, date.day, hour=data['hour'], minute=data['minute'])
+            end_at = datetime.datetime(record.start_at.year, record.start_at.month, record.start_at.day, 
+                                        hour=data['hour'], minute=data['minute'])
 
-            day_total_time = end_at - record.start_at
-            if day_total_time.days < 0:
+            oneday_time = end_at - record.start_at
+            if oneday_time.days < 0:
                 return JsonResponse({'message': 'RECHECK_ENDTIME_ERROR'}, status=400)
+            
+            record.end_at      = end_at
+            record.oneday_time = oneday_time.seconds
+            record.save()
+
+            if not user.average_end:
+                end_seconds      = (end_at-standard_time).total_seconds()
+                user.average_end = str(datetime.timedelta(seconds=end_seconds))
             else:
-                record.end_at      = end_at
-                record.oneday_time = day_total_time.seconds
-                record.save()
-                user.total_time   += day_total_time.seconds
-                user.save()
+                average_end      = end_at.replace(
+                                    hour=user.average_end.hour,     minute=user.average_end.minute, 
+                                    second=user.average_end.second, microsecond=user.average_end.microsecond)
+                end_seconds      = ((average_end-standard_time).total_seconds() + (end_at-standard_time).total_seconds()) / 2
+                user.average_end = str(datetime.timedelta(seconds=end_seconds))
+
+            user.total_time += oneday_time.seconds
+            user.save()
+            user.batch.total_time += oneday_time.seconds
+            user.batch.save()
 
             return JsonResponse({'message': 'SUCCESS'}, status=201)
 
@@ -65,17 +77,28 @@ class RecordStartTimeView(View):
     @login_required
     @check_ip
     def post(self, request):
-        user      = request.user
-        record    = user.record_set.last()
-        now       = datetime.datetime.now()
-        time_gap  = datetime.timedelta(seconds=32406)
-        now_korea = now + time_gap
+        user          = request.user
+        record        = user.record_set.last()
+        now_korea     = datetime.datetime.now() + datetime.timedelta(seconds=32406)
+        standard_time = now_korea.replace(hour=0, minute=0, second=0, microsecond=0)
         
         if record:
             if now_korea.date() == record.start_at.date():
                 return JsonResponse({'message': 'ALREADY_RECORD_ERROR'}, status=400)
 
         Record.objects.create(user_id=user.id, start_at=now_korea)
+
+        if not user.average_start:
+            start_seconds      = (now_korea-standard_time).total_seconds()
+            user.average_start = str(datetime.timedelta(seconds=start_seconds))
+        else:
+            average_start      = now_korea.replace(
+                                    hour=user.average_start.hour,     minute=user.average_start.minute, 
+                                    second=user.average_start.second, microsecond=user.average_start.microsecond)
+            start_seconds      = ((average_start-standard_time).total_seconds() + (now_korea-standard_time).total_seconds()) / 2
+            user.average_start = str(datetime.timedelta(seconds=start_seconds))
+
+        user.save()
 
         result = {
                     'user_id'   : user.id,
@@ -89,11 +112,10 @@ class RecordStopTimeView(View):
     @login_required
     @check_ip
     def post(self, request):
-        user      = request.user
-        record    = user.record_set.last()
-        now       = datetime.datetime.now()
-        time_gap  = datetime.timedelta(seconds=32406)
-        now_korea = now + time_gap
+        user          = request.user
+        record        = user.record_set.last()
+        now_korea     = datetime.datetime.now() + datetime.timedelta(seconds=32406)
+        standard_time = record.start_at.replace(hour=0, minute=0, second=0, microsecond=0)
 
         if not record:
             return JsonResponse({'message': 'NEED_TO_RECORD_STARTTIME_ERROR'}, status=400)
@@ -104,20 +126,32 @@ class RecordStopTimeView(View):
 
         record.end_at = now_korea
 
-        day_total_time = record.end_at - record.start_at
-        if 0 < day_total_time.seconds <= 60:
+        oneday_time = record.end_at - record.start_at
+        if 0 < oneday_time.seconds <= 60:
             return JsonResponse({'message': 'CLOSE_TIME_ERROR'}, status=400)
+        
+        record.oneday_time = oneday_time.seconds
+        record.save()
+
+        if not user.average_end:
+            end_seconds      = (now_korea-standard_time).total_seconds()
+            user.average_end = str(datetime.timedelta(seconds=end_seconds))
         else:
-            record.oneday_time = day_total_time.seconds
-            record.save()
-            user.total_time   += day_total_time.seconds
-            user.save()
+            average_end      = now_korea.replace(
+                                hour=user.average_end.hour,     minute=user.average_end.minute, 
+                                second=user.average_end.second, microsecond=user.average_end.microsecond)
+            end_seconds      = ((average_end-standard_time).total_seconds() + (now_korea-standard_time).total_seconds()) / 2
+            user.average_end = str(datetime.timedelta(seconds=end_seconds))
+
+        user.total_time += oneday_time.seconds
+        user.save()
+        user.batch.total_time += oneday_time.seconds
+        user.batch.save()
             
         result = {
-                    'user_id'          : user.id,
-                    'user_name'        : user.name,
-                    'user_oneday_time' : record.oneday_time,
-                    'comment'          : Message.objects.all().order_by('?').first().content
+                    'user_id'   : user.id,
+                    'user_name' : user.name,
+                    'comment'   : Message.objects.all().order_by('?').first().content
         }
 
         return JsonResponse({'result': result}, status=201)
--- users/views.py
+++ users/views.py
@@ -149,36 +149,19 @@ class StudentView(View):
             return JsonResponse({'message': 'UNAUTHORIZED_USER_ERROR'}, status = 400)
 
         user      = User.objects.select_related('batch').prefetch_related('record_set').get(id=user.id)
-        records   = user.record_set.filter(user_id=user.id)
-        now       = datetime.datetime.now()
-        time_gap  = datetime.timedelta(seconds=32406)
-        now_korea = now + time_gap
+        records   = user.record_set.all()
+        now_korea = datetime.datetime.now() + datetime.timedelta(seconds=32406)
 
-        start_sum   = 0
-        start_count = 0
-        end_sum     = 0
-        end_count   = 0
-
-        for record in records:
-            standard_time     = record.start_at.replace(hour=0, minute=0, second=0, microsecond=0)
-
-            if record.start_at:
-                total         = (record.start_at-standard_time).total_seconds()
-                start_sum    += total
-                start_count  += 1
-            if record.end_at:
-                total         = (record.end_at-standard_time).total_seconds()
-                end_sum      += total
-                end_count    += 1
+        WEEK_DAYS = 5
 
         today          = now_korea.isocalendar()
         week_start_day = date.fromisocalendar(today.year, today.week, 1)
         week_end_day   = week_start_day + datetime.timedelta(days=6)
         week_records   = records.filter(end_at__date__range=[week_start_day, week_end_day])
 
-        weekly_records     = {f'{record.end_at.weekday()}': record.oneday_time for record in week_records}
-        full_weekly_record = {week_number : weekly_records[str(week_number)] \
-                                if str(week_number) in weekly_records.keys() else 0 for week_number in range(5)}
+        week_oneday_time       = {f'{record.end_at.weekday()}': record.oneday_time for record in week_records}
+        total_week_oneday_time = {week_number : week_oneday_time[str(week_number)] \
+                                if str(week_number) in week_oneday_time.keys() else 0 for week_number in range(WEEK_DAYS)}
                     
         total_accumulate_record_result = []
         oneday_time_sum = 0
@@ -195,13 +178,11 @@ class StudentView(View):
                                 'user_total_time'        : user.total_time
                     },
                     'record_information' : {
-                                'weekly_record'            : full_weekly_record,
+                                'weekly_record'            : total_week_oneday_time,
                                 'total_accumulate_records' : total_accumulate_record_result,
-                                'average_start_time'       : str(datetime.timedelta(seconds=start_sum/start_count)) \
-                                                                if not start_count == 0 else 0,
-                                'average_end_time'         : str(datetime.timedelta(seconds=end_sum/end_count)) \
-                                                                if not end_count == 0 else 0,
-                                'wecode_d_day'             : (now_korea.date()-user.batch.start_day).days
+                                'average_start_time'       : str(user.average_start) if user.average_start else 0,
+                                'average_end_time'         : str(user.average_end) if user.average_end else 0,
+                                'wecode_d_day'             : (now_korea.date() - user.batch.start_day).days
 
                     }
         }
@@ -216,21 +197,13 @@ class BatchView(View):
         if user.user_type_id == 2 and not user.batch.id == batch_id:
             return JsonResponse({'message': 'NOT_YOUR_BATCH_ERROR'}, status = 400)
 
+        winner_batch    = Batch.objects.all().order_by('-total_time').first()
         my_batch        = Batch.objects.get(id=batch_id)
         my_batch_users  = User.objects.filter(batch_id=my_batch.id).order_by('name')
         my_batch_mentor = User.objects.get(name=my_batch.mentor_name, user_type_id=1) \
                             if User.objects.filter(name=my_batch.mentor_name, user_type_id=1).exists() else None
-        now             = datetime.datetime.now()
-        time_gap        = datetime.timedelta(seconds=32406)
-        now_korea       = now + time_gap
-
-        all_batches       = Batch.objects.all()
-        batch_total_times = []
-        for batch in all_batches:
-            batch_users = User.objects.filter(batch_id=batch.id)
-            batch_total_times.append(sum([user.total_time for user in batch_users]))
-        winner_total_time = max(batch_total_times)
-
+        now_korea       = datetime.datetime.now() + datetime.timedelta(seconds=32406)
+        
         GOST_RANKING = 3
 
         today               = now_korea.isocalendar()
@@ -242,41 +215,36 @@ class BatchView(View):
             records              = user.record_set.filter(end_at__date__range=[last_week_start_day, last_week_end_day])
             last_week_total_time = 0
             for record in records:
-                last_week_total_time += record.oneday_time
+                last_week_total_time += record.oneday_time if record.oneday_time else 0
             compare_times.append(last_week_total_time)
         
         ranking_results = []
-        if sum(compare_times) == 0:
-            ranking_results = []
+        if len(compare_times) < GOST_RANKING:
+            ranking_times = sorted(compare_times, reverse=True)
         else:
-            if len(compare_times) < GOST_RANKING:
-                ranking_times = sorted(compare_times, reverse=True)
-            else:
-                ranking_times = sorted(compare_times, reverse=True)[:GOST_RANKING]
+            ranking_times = sorted(compare_times, reverse=True)[:GOST_RANKING]
 
-            for ranking_time in ranking_times:
+        for ranking_time in ranking_times:
+            if not ranking_time == 0:
                 user_informaion = {}
                 index_number = compare_times.index(ranking_time)
                 user_informaion['user_id']                   = my_batch_users[index_number].id
                 user_informaion['user_name']                 = my_batch_users[index_number].name
-                user_informaion['user_profile_image_url']    = my_batch_users[index_number].profile_image_url
                 user_informaion['user_last_week_total_time'] = ranking_time
                 ranking_results.append(user_informaion)
         
         result = {
                     'winner_batch_information' : {
-                                'winner_batch_name'       : all_batches[batch_total_times.index(winner_total_time)].name,
-                                'winner_batch_total_time' : winner_total_time
+                                'winner_batch_name'       : winner_batch.name,
+                                'winner_batch_total_time' : winner_batch.total_time
                     },
                     'my_batch_information' : {
                                 'batch_id'         : my_batch.id,
                                 'batch_name'       : my_batch.name,
-                                'batch_total_time' : sum([user.total_time for user in my_batch_users]),
+                                'batch_total_time' : my_batch.total_time,
                                 'ghost_ranking'    : ranking_results,
                                 'peers' : [
                                         {
-                                            'batch_id'               : my_batch.id,
-                                            'batch_name'             : my_batch.name,
                                             'peer_id'                : user.id,
                                             'peer_name'              : user.name,
                                             'peer_profile_image_url' : user.profile_image_url,
@@ -286,28 +254,20 @@ class BatchView(View):
                                             'peer_github'            : user.github if user.github else None,
                                             'peer_birthday'          : user.birthday if user.birthday else None,
                                             'peer_status'            : False if not user.record_set.last() \
-                                                else True if now_korea.date() == user.record_set.last().start_at.date() \
-                                                and not user.record_set.last().end_at else False,
+                                                                        else True if now_korea.date() == user.record_set.last().start_at.date() \
+                                                                        and not user.record_set.last().end_at else False,
                                         } for user in my_batch_users
                                 ],
                                 'mentor' : 
                                         {
-                                            'mentor_id'                : my_batch_mentor.id \
-                                                                            if my_batch_mentor else None,
-                                            'mentor_name'              : my_batch_mentor.name \
-                                                                            if my_batch_mentor else my_batch.mentor_name,
-                                            'mentor_profile_image_url' : my_batch_mentor.profile_image_url \
-                                                                            if my_batch_mentor else None,
-                                            'mentor_position'          : my_batch_mentor.position.name \
-                                                                            if my_batch_mentor else None,
-                                            'mentor_email'             : my_batch_mentor.email \
-                                                                            if my_batch_mentor else None,
-                                            'mentor_blog'              : my_batch_mentor.blog \
-                                                                            if my_batch_mentor else None,
-                                            'mentor_github'            : my_batch_mentor.github \
-                                                                            if my_batch_mentor else None,
-                                            'mentor_birthday'          : my_batch_mentor.birthday \
-                                                                            if my_batch_mentor else None
+                                            'mentor_id'                : my_batch_mentor.id if my_batch_mentor else None,
+                                            'mentor_name'              : my_batch_mentor.name if my_batch_mentor else my_batch.mentor_name,
+                                            'mentor_profile_image_url' : my_batch_mentor.profile_image_url if my_batch_mentor else None,
+                                            'mentor_position'          : my_batch_mentor.position.name if my_batch_mentor else None,
+                                            'mentor_email'             : my_batch_mentor.email if my_batch_mentor else None,
+                                            'mentor_blog'              : my_batch_mentor.blog if my_batch_mentor else None,
+                                            'mentor_github'            : my_batch_mentor.github if my_batch_mentor else None,
+                                            'mentor_birthday'          : my_batch_mentor.birthday if my_batch_mentor else None
                                         }
                     }
         }
@@ -323,22 +283,16 @@ class BatchListView(View):
         if not user.user_type.id == 1:
             return JsonResponse({'message': 'UNAUTHORIZED_USER_ERROR'}, status = 400)
 
-        now       = datetime.datetime.now()
-        time_gap  = datetime.timedelta(seconds=32406)
-        now_korea = now + time_gap
-        
+        now_korea     = datetime.datetime.now() + datetime.timedelta(seconds=32406)        
         total_results = []
 
         for batch in batches:
             on_user_number   = 0
-            user_total_times = []
             users            = User.objects.filter(batch_id=batch.id)
-
-            batch_mentor = User.objects.get(name=batch.mentor_name, user_type_id=1) \
-                            if User.objects.filter(name=batch.mentor_name, user_type_id=1).exists() else None
+            batch_mentor     = User.objects.get(name=batch.mentor_name, user_type_id=1) \
+                                if User.objects.filter(name=batch.mentor_name, user_type_id=1).exists() else None
 
             for user in users:
-                user_total_times.append(user.total_time)
                 if not user.record_set.last():
                     on_user_number += 0
                 elif now_korea.date() == user.record_set.last().start_at.date() and not user.record_set.last().end_at:
@@ -353,10 +307,10 @@ class BatchListView(View):
                         'mentor_name'             : batch_mentor.name if batch_mentor else batch.mentor_name,
                         'batch_start_day'         : batch.start_day,
                         'batch_end_day'           : batch.end_day,
-                        'batch_total_time'        : sum(user_total_times),
-                        'wecode_d_day'            : (now_korea.date()-batch.start_day).days,
+                        'batch_total_time'        : batch.total_time,
+                        'wecode_d_day'            : (now_korea.date() - batch.start_day).days,
                         'batch_on_user_number'    : on_user_number,
-                        'batch_total_user_number' : len(User.objects.filter(batch_id=batch.id))
+                        'batch_total_user_number' : len(users)
             }
             
             total_results.append(result)
@@ -430,6 +384,7 @@ class ModifyBatchView(View):
     def delete(self, request, batch_id):
         if User.objects.filter(batch_id=batch_id).exists():
             return JsonResponse({'message': 'ALREADY_USED_ERROR'}, status=400)
+            
         Batch.objects.get(id=batch_id).delete()
 
         return JsonResponse({'message': 'SUCCESS'}, status=204)
