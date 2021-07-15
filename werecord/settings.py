f4d226acf0c8fabf851dc9212622dcec5e83c6bc
hwaya2828 <hwaya2828@gmail.com>
2021-07-15 11:05:31 +0900

    FIX: SETTING 변경

--- werecord/settings.py
+++ werecord/settings.py
@@ -4,7 +4,6 @@ from my_settings import DATABASES, SECRET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -15,7 +14,6 @@ SECRET_KEY = SECRET['secret']
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -62,12 +60,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'werecord.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = DATABASES
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -87,7 +83,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -100,7 +95,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = False
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
