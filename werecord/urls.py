985e600bddf5ba75154976d1b7fa6af76a18a256
강경훈 <kyunghun@gang-gyeonghuns-MacBook-Pro.local>
2021-10-23 15:40:40 +0900

    [ADD] Ping api

--- werecord/urls.py
+++ werecord/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 
+from ping        import PingView
+
 urlpatterns = [
     path("users", include("users.urls")),
     path("records", include("records.urls")),
+    path("ping", PingView.as_view())
 ]
